9e3285aa2e26e3cb93f3c12df9bb2fa67ca6d01f
manishvbn <manishvbn@gmail.com>
2023-05-03 10:45:15 +0530

    CommonJS Modules Lab

---
+++ Day1/2_user-defined-modules/1_modules-commonjs/app.js
@@ -0,0 +1,11 @@
+// Import an entire module for side effects only, without importing anything. 
+// This runs the module's global code, but doesn't actually import any values.
+// require('./lib.js');
+
+// const lib = require('./lib.js');
+// // console.log(lib);
+// console.log(lib.firstname);
+// console.log(lib.lastname);
+// console.log(lib.hello("Hi from App Module"));
+
+// Load and instantiate the Employee Object  
