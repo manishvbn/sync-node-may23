a88fa256631a9af44a721a674a7b7c4610ebc3e3
manishvbn <manishvbn@gmail.com>
2023-05-04 10:42:00 +0530

    NPM Packaging

---
+++ Day2/2_folder-watcher/app.js
@@ -0,0 +1,19 @@
+// const chokidar = require('chokidar');
+
+// const watcher = chokidar.watch('./my-folder');
+
+// watcher.on('add', (path) => { console.log(`File, ${path}, is created...`); });
+// watcher.on('change', (path) => { console.log(`File, ${path}, is modified...`); });
+// watcher.on('unlink', (path) => { console.log(`File, ${path}, is deleted...`); });
+
+// console.log("Chokidar is watching....");
+
+const _ = require('lodash');
+
+var employees = [{ id: 1, name: "Manish" },
+{ id: 2, name: "Abhijeet" },
+{ id: 3, name: "Ram" },
+{ id: 4, name: "Abhishek" },
+{ id: 5, name: "Ramakant" }];
+
+console.log(_.last(employees));
