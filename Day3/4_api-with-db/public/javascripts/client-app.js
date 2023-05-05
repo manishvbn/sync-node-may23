facba6acedde385e2fa1754f8944ee34f175b788
manishvbn <manishvbn@gmail.com>
2023-05-05 14:42:53 +0530

    Client Code to Consume API

--- Day3/4_api-with-db/public/javascripts/client-app.js
+++ Day3/4_api-with-db/public/javascripts/client-app.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
     $('#btnLoad').click(function () {
-        alert("Button was clicked...");
+        // alert("Button was clicked...");
+
+        $('#tBody').empty();
+
+        $.ajax({
+            url: 'http://localhost:3000/api/employees',
+            type: 'GET',
+            dataType: 'json',
+            success: function (resData) {
+                if (resData.data.length > 0) {
+                    var tmpl = $.templates('#employeeRowTemplate');
+                    var html = tmpl.render(resData.data);
+                    $('#tBody').append(html);
+                }
+            }, error: function (err) {
+                console.error(err);
+            }
+        });
     });
 });
